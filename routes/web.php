4f2a9f13b72bbce8ff3c0ab09c8cd58af804d4da
BenardMN <benardmunuve934@gmail.com>
2023-11-27 16:06:49 +0300

    fir err

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dash', function () {
+    return view('dash');
+});
+
+//load index page
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+//load login
+Route::get('/login', function () {
+    return view('login');
+});
+
+//load register page
+Route::get('/register', function () {
+    return view('register');
+});
+
+//load user profile
+Route::get('/profile', function () {
+    return view('/profile');
 });
